f932ee8272ad19e11ff8c0897714a632c249bed8
JessieMidgley <jessiemidgley@gmail.com>
2024-10-23 09:12:01 +0200

    Remove all .command.* and .exitcode files from tracking and update .gitignore

--- day6/work/b8/9529a66d20b17ccff3e26946029b0a/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-printf 'Hello World' | split -b 4 - h_w_chunk_
--- day6/work/db/acce02680d52ba99b9d53f25bcafc9/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-zip greeting_uppercase.zip greeting_uppercase.txt
-
-gzip -k greeting_uppercase.txt -c > greeting_uppercase.txt.gz
-
-bzip2 -k greeting_uppercase.txt -c > greeting_uppercase.txt.bz2
